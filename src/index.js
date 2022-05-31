d60a416f5338d275ee904c4530d4dbcc07fe3a99
MaxBerezhnoi <92980454+MaxBerezhnoi@users.noreply.github.com>
2022-05-31 22:14:37 +0300

    31.05.22

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+//Наработка по видео
+console.log(fetch('https://restcountries.com/v3.1/name/peru').then(response => {
+    console.log(response.json());
+    return response.json();
+}).then(country => {
+    console.log(country);
+
+}).catch(error => {
+    console.log(error);
+    Notify.info("Too many matches found. Please enter a more specific name.");
+}));
+//-----------------------------------
 
 const DEBOUNCE_DELAY = 300;
+const refs = {
+    input: document.querySelector('#search-box'),
+}
+
+refs.input.addEventListener("input", fetchCountries);
+
+fetchCountries().then().catch(error => {
+    console.log(error);
+    Notify.info("Too many matches found. Please enter a more specific name.");
+})
+
+function fetchCountries(e) {
+    let name = e.currentTarget.textCntent;
+    return fetch(`https://restcountries.com/v3.1/name/${name}`)
+        .then(response => {
+            return response.json();
+        },
+        );
+}
+
+
+        /*.then(name => {
+            console.log({ name });
+        })*/
+        
+    

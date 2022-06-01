87ffa902178b55b41ec29380ef7d55de8a0a38d0
MaxBerezhnoi <92980454+MaxBerezhnoi@users.noreply.github.com>
2022-06-01 22:55:31 +0300

    вывод HTML

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import countryInfo from './templates/countryInfo.hbs';
 
 //Наработка по видео
-console.log(fetch('https://restcountries.com/v3.1/name/peru').then(response => {
+/*console.log(fetch('https://restcountries.com/v3.1/name/peru').then(response => {
     console.log(response.json());
     return response.json();
 }).then(country => {
@@ -11,33 +13,49 @@ console.log(fetch('https://restcountries.com/v3.1/name/peru').then(response => {
 }).catch(error => {
     console.log(error);
     Notify.info("Too many matches found. Please enter a more specific name.");
-}));
+}));*/
 //-----------------------------------
 
 const DEBOUNCE_DELAY = 300;
 const refs = {
+    countryInfo: document.querySelector(".country-info"),
     input: document.querySelector('#search-box'),
 }
 
-refs.input.addEventListener("input", fetchCountries);
+refs.input.addEventListener("input", debounce(fetchCountries, DEBOUNCE_DELAY));
 
-fetchCountries().then().catch(error => {
+/*fetchCountries().then(country => {
+    console.log(country);
+}).catch(error => {
     console.log(error);
     Notify.info("Too many matches found. Please enter a more specific name.");
 })
-
+*/
 function fetchCountries(e) {
-    let name = e.currentTarget.textCntent;
-    return fetch(`https://restcountries.com/v3.1/name/${name}`)
-        .then(response => {
-            return response.json();
-        },
-        );
+    if (refs.input.value === "") {
+        console.log("Let's start!");
+        return;
+    }
+    else {
+        let name = e.target.value;
+    
+        console.log(name);
+        return fetch(`https://restcountries.com/v3.1/name/${name}`)
+            .then(response => {
+                //console.log(response.json());
+                return response.json();
+            },
+            )
+            .then(countryFullInfo => {
+                console.log(countryFullInfo);
+                const markup = countryInfo(countryFullInfo);
+                console.log(markup);
+                refs.countryInfo.innerHTML = markup;
+            })
+            .catch(error => {
+                console.log(error);
+                Notify.failure("Oops, there is no country with that name");
+            })
+    }
 }
 
-
-        /*.then(name => {
-            console.log({ name });
-        })*/
-        
-    

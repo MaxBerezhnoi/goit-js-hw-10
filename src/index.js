7f0e7507e2d3c332bc271f0c60b7f2a3d0bd3165
MaxBerezhnoi <92980454+MaxBerezhnoi@users.noreply.github.com>
2022-06-04 00:43:47 +0300

    вывод данных

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 import countryInfo from './templates/countryInfo.hbs';
+import countryList from './templates/countryList.hbs';
 
 //Наработка по видео
 /*console.log(fetch('https://restcountries.com/v3.1/name/peru').then(response => {
@@ -20,49 +21,57 @@ const DEBOUNCE_DELAY = 300;
 
 
 const refs = {
+    countryListUl: document.querySelector(".country-list"),
     countryInfoDiv: document.querySelector(".country-info"),
     input: document.querySelector('#search-box'),
+    list: document.querySelector("li"),
 }
 
+    
 refs.input.addEventListener("input", debounce(fetchCountries, DEBOUNCE_DELAY));
 
-/*fetchCountries().then(country => {
-    console.log(country);
-}).catch(error => {
-    console.log(error);
-    Notify.info("Too many matches found. Please enter a more specific name.");
-})
-*/
+function clearCountriesBlock() {
+    refs.countryListUl.innerHTML = "";
+    refs.countryInfoDiv.innerHTML = "";
+}
 function fetchCountries(e) {
     if (refs.input.value === "") {
         console.log("Let's start!");
+        clearCountriesBlock();
         return;
     }
     else {
         let name = e.target.value.trim();
     
         console.log(name);
-        return fetch(`https://restcountries.com/v3.1/name/${name}`)
+        return fetch(`https://restcountries.com/v3.1/name/${name}`
+            
+        )
             .then(response => {
-                //console.log(response.json());
                 return response.json();
             },
             )
             .then(countryFullInfo => {
                 
                 if (countryFullInfo.length === 1) {
+                    refs.countryListUl.innerHTML = "";
                     console.log(countryFullInfo);
                     const markup = countryInfo(countryFullInfo);
                     console.log(markup);
                     refs.countryInfoDiv.innerHTML = markup;
+                }
+                else if (countryFullInfo.length <= 10) {
+                    
+                    const markup = countryList(countryFullInfo);
+                    refs.countryListUl.innerHTML = markup;
                 }
                 else if (countryFullInfo.length > 10) {
                     console.log("Too match!");
                     Notify.info("Too many matches found. Please enter a more specific name.");
             }
                 else {
-                   Notify.failure("Oops, there is no country with that name"); 
-               } 
+                    Notify.failure("Oops, there is no country with that name"); 
+                } 
                 
             })
             .catch(error => {
